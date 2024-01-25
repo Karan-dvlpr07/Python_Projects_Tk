2754c7caf0e3e92214459b23fb650bb41db180cb
Karan <150646218+Karan-dvlpr07@users.noreply.github.com>
2024-01-25 14:05:22 +0530

    Add files via upload

---
+++ Project_1/Tk_Fram.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import tkinter as tk
+
+root = Tk()
+root.geometry('400x400')
+
+# S = Label(root, text = "Hello world" , font=40 , background="orange", fg="blue" )
+# S.pack()
+
+# b = Button(root, text="click here", activebackground= "orange", activeforeground= "white" )
+# b.pack()
+root.mainloop()
