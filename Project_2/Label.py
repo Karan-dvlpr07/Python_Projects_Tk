fde3c5ea387729c2ee326a93c61f995290aa9bf8
Karan <150646218+Karan-dvlpr07@users.noreply.github.com>
2024-01-26 05:30:04 +0530

    Add files via upload

---
+++ Project_2/Label.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter as tk
+
+root = Tk()
+root.geometry('400x400')
+
+S = Label(root, text = "Hello world" , font=40 , background="orange", fg="blue" )
+S.pack()
+
+root.mainloop()
